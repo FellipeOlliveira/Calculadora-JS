adc98d41c1436fd70b78c73745e6b21be3a4548b
Marcos <marcosf1625@gmail.com>
2024-02-27 08:41:48 -0300

    feat: calcular

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const mostruario = document.getElementById('resultado')
 const escrever = (number) => {
     mostruario.innerHTML += number
 }
+
+const calcular = () => {
+    let macaco = mostruario.innerHTML
+    let calculo = eval(macaco)
+    console.log(mostruario.innerHTML)
+    console.log(calculo)
+
+    mostruario.innerHTML = calculo
+}
